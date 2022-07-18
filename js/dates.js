9851f7c1251e5d109c6f8853b5124fda5a2c67ce
Olga Naboichenko <olga140404ol@gmail.com>
2022-07-18 14:13:58 +0300

    home task 15

--- js/dates.js
+++ js/dates.js
@@ -23,12 +23,22 @@ function getDay(date, lang) {
       6: 'Суббота',
     },
   };
+  return dayNames[lang][date.getDay()];
 }
 
 // Принимает объект даты, и должно вернуть компоненты даты в виде строки.
 // Вид должен быть такой 12:02(часы и минуты), то есть если у вас одно число на одном из
 // компонентов, то добавляем 0 перед ним
-function formatTime(date) {}
+function formatTime(date) {
+  var time = new Date(date);
+  var hour = time.getHours();
+  var minute = time.getMinutes();
+  var temp = "" + hour;
+  if (hour == 0)
+    temp = "12";
+  temp += ((minute < 10) ? ":0" : ":") + minute;
+  return temp;
+}
 
 /*
 Напишите функцию getLastDayOfMonth(year, month), 
@@ -39,7 +49,9 @@ year – год из четырёх цифр, например, 2012.
 month – месяц от 0 до 11.
 К примеру, getLastDayOfMonth(2012, 1) = 29 (високосный год, февраль).
 */
-function getLastDayOfMonth(year, month) {}
+function getLastDayOfMonth(year, month) {
+  return new Date(year, month + 1, 0).getDate();
+}
 
 module.exports = {
   getDay,
